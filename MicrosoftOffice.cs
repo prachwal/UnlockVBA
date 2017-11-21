2e54e218077dcd73fe178efeb00822f129d77ca8
Przemysław Rachwał <przemyslaw.rachwal@gmail.com>
2017-11-21 22:25:07 +0100

    Dodaj pliki projektów.

---
+++ MicrosoftOffice.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+
+namespace UnlockVBA
+{
+    internal class MicrosoftOffice
+    {
+        public MicrosoftOffice(string fileIn, string fileOut) : this()
+        {
+            FileIn = fileIn;
+            FileOut = fileOut;
+        }
+
+        public MicrosoftOffice()
+        {
+            _sourceSeq = new byte[] { 0x44, 0x50, 0x42 };
+            _targetSeq = new byte[] { 0x44, 0x50, 0x78 };
+        }
+
+        public string FileIn { get; }
+        public string FileOut { get; }
+        private readonly byte[] _sourceSeq;
+
+        private readonly byte[] _targetSeq;
+
+        public void Decode()
+        {
+            if (File.Exists(FileOut))
+            {
+                File.Delete(FileOut);
+            }
+
+            FileStream sourceStream = File.OpenRead(FileIn);
+            FileStream targetStream = File.Create(FileOut);
+
+            try
+            {
+                int b;
+                long foundSeqOffset = -1;
+                int searchByteCursor = 0;
+
+                while ((b = sourceStream.ReadByte()) != -1)
+                {
+                    if (_sourceSeq[searchByteCursor] == b)
+                    {
+                        if (searchByteCursor == _sourceSeq.Length - 1)
+                        {
+                            targetStream.Write(_targetSeq, 0, _targetSeq.Length);
+                            searchByteCursor = 0;
+                            foundSeqOffset = -1;
+                        }
+                        else
+                        {
+                            if (searchByteCursor == 0)
+                            {
+                                foundSeqOffset = sourceStream.Position - 1;
+                            }
+
+                            ++searchByteCursor;
+                        }
+                    }
+                    else
+                    {
+                        if (searchByteCursor == 0)
+                        {
+                            targetStream.WriteByte((byte)b);
+                        }
+                        else
+                        {
+                            targetStream.WriteByte(_sourceSeq[0]);
+                            sourceStream.Position = foundSeqOffset + 1;
+                            searchByteCursor = 0;
+                            foundSeqOffset = -1;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                sourceStream.Dispose();
+                targetStream.Dispose();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnlockVBA
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var app = new CommandLineApplication
+                {
+                    Name = "UnlockVBA"
+                };
+                app.HelpOption("-?|-h|--help");
+
+                app.Command("unlock", (command) =>
+                {
+                    command.Description = "Decode VBA protection in Microsoft Office file";
+                    command.HelpOption("-?|-h|--help");
+
+                    var fileInOption = command.Option("-in|--FileIn", "File in path", CommandOptionType.SingleValue);
+                    var fileOutOption = command.Option("-out|--FileOut", "File out path", CommandOptionType.SingleValue);
+
+                    command.OnExecute(() =>
+                    {
+                        var fileIn = fileInOption.Values.FirstOrDefault();
+                        var fileOut = fileOutOption.Values.FirstOrDefault();
+                        var c = new MicrosoftOffice(fileIn, fileOut);
+                        c.Decode();
+                        return 0;
+                    });
+                });
+
+                app.Command("hide", (command) =>
+                {
+                    command.Description = "Instruct the ninja to hide in a specific location.";
+                    command.HelpOption("-?|-h|--help");
+
+                    var locationArgument = command.Argument("[location]",
+                        "Where the ninja should hide.");
+
+                    command.OnExecute(() =>
+                    {
+                        var location = locationArgument.Values.Any() ?
+                            String.Join(", ", locationArgument.Values.ToArray()) :
+                            "under a turtle";
+                        Console.WriteLine("Ninja is hidden " + location);
+                        return 0;
+                    });
+                });
+
+                app.Command("attack", (command) =>
+                {
+                    command.Description = "Instruct the ninja to go and attack!";
+                    command.HelpOption("-?|-h|--help");
+
+                    var excludeOption = command.Option("-e|--exclude <exclusions>",
+                        "Things to exclude while attacking.",
+                        CommandOptionType.MultipleValue);
+
+                    var screamOption = command.Option("-s|--scream",
+                        "Scream while attacking",
+                        CommandOptionType.NoValue);
+
+                    command.OnExecute(() =>
+                    {
+                        var exclusions = excludeOption.Values;
+                        var attacking = (new List<string> {
+                            "dragons",
+                            "badguys",
+                            "civilians",
+                            "animals"
+                        }).Where(x => !exclusions.Contains(x));
+
+                        Console.Write("Ninja is attacking " + string.Join(", ", attacking));
+
+                        if (screamOption.HasValue())
+                        {
+                            Console.Write(" while screaming");
+                        }
+
+                        Console.WriteLine();
+
+                        return 0;
+                    });
+                });
+
+                app.Execute(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
